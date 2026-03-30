485bba318a1104ef7c4fd55fe1f6c1838cbdafef
Tanvi <159919214+TanviKrishnan2005@users.noreply.github.com>
2026-03-30 17:48:27 +0530

    Add: reorder-linked-list - submission-0

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        //finding middle using slow and fast pointer
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        //reverse the second half;(whereslow is that is the mid)
+        ListNode* second = slow->next;
+        ListNode* prev =slow->next=NULL;
+
+        while(second!=NULL){
+            ListNode* temp = second->next;
+            second->next = prev;
+            prev= second;
+            second= temp;
+        }
+
+        //merge both lists now
+
+        ListNode* first = head;
+        second = prev;
+        while(second!=NULL){
+            ListNode* temp1 = first->next;
+            ListNode* temp2 = second->next;
+            first->next = second;
+            second->next = temp1;
+            first = temp1;
+            second = temp2;
+        }
+    }
+};
