81147f80f736c336d1e7913fe4a44ee9b2ea0541
Tanvi <159919214+TanviKrishnan2005@users.noreply.github.com>
2026-04-12 16:01:12 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l = 0;
+        int r = s.size() - 1;
+
+        while (l < r) {
+            if (!isalphanum(s[l])) {
+                l++;
+                continue;
+            }
+            if (!isalphanum(s[r])) {
+                r--;
+                continue;
+            }
+
+            if (tolower(s[l]) != tolower(s[r])) {
+                return false;
+            }
+
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    bool isalphanum(char c) {
+        return (c >= 'A' && c <= 'Z') ||
+               (c >= 'a' && c <= 'z') ||
+               (c >= '0' && c <= '9');
+    }
+};
