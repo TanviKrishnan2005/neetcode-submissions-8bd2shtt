47785d66001efcf913a90b282fa133032467a745
Tanvi <159919214+TanviKrishnan2005@users.noreply.github.com>
2026-07-16 14:49:47 +0530

    Add: move-zeroes - submission-3

---
+++ Data Structures & Algorithms/move-zeroes/submission-3.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+     for(int i =0, r =0; r<nums.size();r++){
+        if(nums[r]){
+            swap(nums[i],nums[r]);
+            i++;
+            }
+        }   
+    }
+};
