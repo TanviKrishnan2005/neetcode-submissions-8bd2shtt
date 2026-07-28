e92282cae0d9f9b4f6bc22fa5f55a418221c9bdd
Tanvi <159919214+TanviKrishnan2005@users.noreply.github.com>
2026-07-28 20:19:14 +0530

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string>res;
+    void solve(int n , string cur , int open , int close){
+        if(cur.length()== 2*n){
+            res.push_back(cur);
+            return;
+        }
+        if(open<n){
+            cur.push_back('(');
+            solve(n,cur,open+1,close);
+            cur.pop_back();
+        }
+        if(close<open){
+            cur.push_back(')');
+            solve(n,cur,open,close+1);
+            cur.pop_back();
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        string cur = "";
+        solve(n,cur,0,0);
+        return res;
+    }
+};
